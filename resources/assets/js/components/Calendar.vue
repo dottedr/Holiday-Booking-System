053010b9442498644ad1edcd86426b448c61eb1a
Sabina Adamska <sabina@pixl.org.uk>
2018-06-12 14:48:50 +0100

    mock vue calendar

---
+++ resources/assets/js/components/Calendar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row justify-content-center">
+        <full-calendar :config="config" :events="events"/>
+    </div>
+
+</template>
+<style>
+    @import '~fullcalendar/dist/fullcalendar.css';
+</style>
+<script>
+    import moment from 'moment'
+    export default {
+        name: 'calendar',
+        data () {
+            return {
+                events: [
+                    {
+                        title: 'test',
+                        allDay: true,
+                        start: moment(),
+                        end: moment().add(1, 'd'),
+                    },
+                    {
+                        title: 'another test',
+                        start: moment().add(2,'d'),
+                        end: moment().add(2, 'd').add(2, 'h'),
+                    },
+                ],
+                config: {
+                    defaultView: 'month',
+                    eventRender: function(event, element) {
+                        console.log(event)
+                    }
+                },
+            }
+        }
+    }
+
+</script>
