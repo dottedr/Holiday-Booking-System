738251f3d85c38436e93624e092138dbd51c8687
Sabina Adamska <sabina@pixl.org.uk>
2018-06-22 14:43:38 +0100

    ManageRequestViewController

---
+++ app/Http/Controllers/ManageRequestViewController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManageRequestViewController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($hrequestid=null)
+    {
+        if($hrequestid==null){
+        //TODO: return only to admin
+            return view('viewrequests', array("hrequestid"=>$hrequestid));
+        }
+        else{
+            return view('singlerequest', array("hrequestid"=>$hrequestid));
+        }
+    }
+    public function indexMyRequests($hrequestid=null)
+    {
+        return view('myrequests');
+    }
+
+
+
+}
